a08f1e7f22ec4f11979d0bb3747e792f0711c056
alamgir5253 <alamgir5253@gmail.com>
2022-04-04 22:09:50 +0600

    not found style done

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import NFound from '../../image/404-pnf.webp'
 const NotFound = () => {
     return (
-        <div>
-            <h1>404 not found</h1>
+        <div className='  grid grid-cols-1 sm:grid-cols-2 justify-items-center items-center '>
+            <h1 className='text-center py-5 font-bold font-sans text-lg lg:text-4xl md:text-2xl'> sorry... page not found</h1>
+            <img className='w-full h-full' src={NFound} alt="" />
+            
         </div>
     );
 };
